18c098852ed172f23c74bb5f1d504f53c0a0c9c4
fukushima0102@gmail.com <ec2-user@ip-172-31-0-100.ap-northeast-1.compute.internal>
2021-05-08 16:11:31 +0000

    Add_guest_sign_in

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,18 @@
 class HomesController < ApplicationController
+
+  def guest_sign_in
+    user = User.find_or_create_by(email: 'guest@guest') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = 'ゲスト'
+    end
+    sign_in user
+    redirect_to root_path, notice: "ゲストユーザーとしてログインしました"
+  end
+
   def top
   end
 
   def about
   end
+
 end
---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,13 @@
+module DeviseHelper
+  def bootstrap_alert(key)
+    # BootstrapのAlertで使われているクラス名に置き換え
+    case key
+    when "alert"
+      "danger"
+    when "notice"
+      "success"
+    when "error"
+      "danger"
+    end
+  end
+end
