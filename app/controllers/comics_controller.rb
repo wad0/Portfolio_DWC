6360f6d84cebd26ae6b22337704f7a6e2d3325b4
fukushima0102@gmail.com <ec2-user@ip-172-31-0-100.ap-northeast-1.compute.internal>
2021-05-09 00:12:23 +0000

    Add_comic

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,13 +1,39 @@
 class ComicsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+
   def new
+    @comic = Comic.new
+  end
+
+  def create
+    @comic = Comic.new(comic_params)
+    @comic.save
+    redirect_to comics_path
   end
 
   def index
+    @comics = Comic.all.page(params[:page]).per(8)
   end
 
   def show
+    @comic = Comic.find(params[:id])
   end
 
   def edit
   end
+
+  private
+
+  def comic_params
+    params.require(:comic).permit(:title,:memo,:complete,:evaluation)
+  end
+
+  def ensure_correct_user
+    @comic = Comic.find(params[:id])
+    unless @comic.user == current_user
+      redirect_to comics_path
+    end
+  end
+
 end
