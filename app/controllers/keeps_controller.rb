e069a9c90171c86318c1f020200b2119071d1e6e
wad0 <fukushima0102@gmail.com>
2021-05-18 19:35:49 +0000

    Add_top

--- app/controllers/keeps_controller.rb
+++ app/controllers/keeps_controller.rb
@@ -3,13 +3,15 @@ class KeepsController < ApplicationController
   def create
     @keep = current_user.keeps.new(comic_id: params[:comic_id])
     @keep.save
-    redirect_to comics_path, notice:"読みたいリストに追加しました"
+    flash[:notice] = "読みたいリストに追加しました"
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @keep = Keep.find_by(comic_id: params[:comic_id], user_id: current_user.id)
     @keep.destroy
-    redirect_to comics_path, notice:"読みたいリストから削除しました"
+    flash[:notice] = "読みたいリストから削除しました"
+    redirect_back(fallback_location: root_path)
   end
 
 end
