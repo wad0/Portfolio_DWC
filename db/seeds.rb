80259cf9aa799d3188ef95b4ec083ddc50c2e3fa
wad0 <fukushima0102@gmail.com>
2021-05-25 15:46:23 +0000

    Fix_kaminari

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-array = %w(スポーツ ファンタジー 恋愛 学園/青春 ホラー/ミステリー 日常/ほのぼの バトル/アクション ギャグ ロボット 歴史 SF 冒険)
+array = %w(スポーツ ファンタジー 恋愛 学園/青春 ホラー/ミステリー 日常/ほのぼの バトル/アクション ギャグ ロボット 歴史 SF 冒険 お仕事 専門分野 人外 コスプレ ものづくり)
 array.each{ |tag|
   tag_list = ActsAsTaggableOn::Tag.new
   tag_list.name = tag
